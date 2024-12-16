cbb03cab814a7d1a864dbf5fa1d56a837d4ba52d
Tamia Pulfer <tamia.pulfer@t-online.de>
2024-12-16 09:31:24 +0100

    Update and rename StartTrigger to StartTrigger.js

--- StartTrigger.js
+++ StartTrigger.js
@@ -2,19 +2,19 @@ const { DefaultAzureCredential } = require("@azure/identity");
 const { ComputeManagementClient } = require("@azure/arm-compute");
 module.exports = async function (context, myTimer) {
     context.log('Start-VM Function Triggered.');
-    // Azure VM Details
+    
     const subscriptionId = "26f614c0-22f5-442c-b51d-8352ce46e0e7";
     const resourceGroupName = "rg-24-04-on-pulfer-tamiap";
     const vmName = "TimerApp4";
-    // Authenticate with Azure
+    
     const credential = new DefaultAzureCredential();
     const computeClient = new ComputeManagementClient(credential, subscriptionId);
     try {
-        // Start the VM
+        
         context.log(`Starting VM: ${vmName}`);
         await computeClient.virtualMachines.beginStartAndWait(resourceGroupName, vmName);
         context.log(`VM ${vmName} started successfully.`);
     } catch (error) {
         context.log.error(`Error starting VM: ${error.message}`);
     }
-};
+}
