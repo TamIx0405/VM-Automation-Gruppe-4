9eeae4dbbbd80c7af16138a3533a3cc3bcba009e
Tamia Pulfer <tamia.pulfer@t-online.de>
2024-12-16 09:30:42 +0100

    Update and rename StoppTrigger to StoppTrigger.js

--- StoppTrigger.js
+++ StoppTrigger.js
@@ -3,15 +3,15 @@ const { DefaultAzureCredential } = require("@azure/identity");
 const { ComputeManagementClient } = require("@azure/arm-compute");
 module.exports = async function (context, myTimer) {
     context.log('Stop-VM Function Triggered.');
-    // Azure VM Details
+    
     const subscriptionId = "26f614c0-22f5-442c-b51d-8352ce46e0e7";
     const resourceGroupName = "rg-24-04-on-pulfer-tamiap";
     const vmName = "TimerApp4";
-    // Authenticate with Azure
+    
     const credential = new DefaultAzureCredential();
     const computeClient = new ComputeManagementClient(credential, subscriptionId);
     try {
-        // Stop the VM
+        
         context.log(`Stopping VM: ${vmName}`);
         await computeClient.virtualMachines.beginPowerOffAndWait(resourceGroupName, vmName);
         context.log(`VM ${vmName} stopped successfully.`);
